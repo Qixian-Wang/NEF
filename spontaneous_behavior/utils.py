5aae4a2eddb29315d58e5c5cf130b3ce18cb6f4e
Qixian-Wang <wqx1377823969@outlook.com>
2025-05-20 16:39:36 -0500

    modify stdp model

--- spontaneous_behavior/utils.py
+++ spontaneous_behavior/utils.py
@@ -1,16 +1,79 @@
 from brian2 import *
 import matplotlib.pyplot as plt
+import os
+import math
 
-def plot_w(S1M):
-    plt.rcParams["figure.figsize"] = (20, 10)
-    subplot(311)
-    plot(S1M.t / ms, S1M.w.T / 0.5)
-    ylabel('w / wmax')
-    subplot(312)
-    plot(S1M.t / ms, S1M.ge.T / 0.5)
-    ylabel('w / wmax')
-    show()
-#
+def plot_figures(model, spike_time, group_index, n_per_group, n_groups, dt_bin=2*ms, smooth_width=200*ms):
+    save_path = 'plots'
+    os.makedirs(save_path, exist_ok=True)
+
+    # Plot weight
+    plt.figure(figsize=(20, 10))
+    monitor = model['S2_monitor']
+    subplot(211)
+    plot(monitor.t / ms, monitor.w.T)
+    ylabel('synaptic weight')
+    subplot(212)
+    plot(monitor.t / ms, monitor.ge.T)
+    ylabel('synaptic conductance')
+    plt.savefig(os.path.join(save_path, 'weight.png'), format='png')
+    plt.close('all')
+
+    # Plot firing rate
+    monitor = model['excitatory_spike']
+    for fig_index in range(math.ceil(max(monitor.t)/10)):
+        t_all = monitor.t
+        i_all = monitor.i
+        mask = (t_all >= fig_index * 10 * second) & (t_all <= (fig_index+1) * 10 * second)
+
+        t_all_masked = t_all[mask]
+        i_all_masked = i_all[mask]
+
+        group_all = i_all_masked // n_per_group
+
+        t_all_ms = t_all_masked / ms
+        dt_bin_ms = dt_bin / ms
+        bins = np.arange(t_all_ms.min(), t_all_ms.max() + dt_bin_ms, dt_bin_ms)
+
+        counts = np.zeros((n_groups, len(bins) - 1))
+        for g in range(n_groups):
+            mask = (group_all == g)
+            counts[g], _ = np.histogram(t_all_ms[mask], bins=bins)
+
+        bin_sec = dt_bin_ms / 1000.0
+        rates = counts / (n_per_group * bin_sec)
+
+        window_bins = int((smooth_width / ms) / dt_bin_ms)
+        if window_bins > 1:
+            kernel = np.ones(window_bins) / window_bins
+            rates_smooth = np.array([np.convolve(r, kernel, mode='same') for r in rates])
+        else:
+            rates_smooth = rates
+
+        time_centers = bins[:-1] + dt_bin_ms / 2
+        plt.figure(figsize=(20, 6))
+        plt.plot(time_centers, rates_smooth.mean(axis=0))
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Firing rate (Hz)')
+        plt.title('Grouped firing rates')
+        plt.savefig(os.path.join(save_path, f'firing_rate{fig_index}.png'), format='png')
+        plt.close('all')
+
+    # Plot rastor plot
+    for fig_index in range(math.ceil(max(spike_time)/10)):
+        mask = (spike_time >= fig_index * 10) & (spike_time <= (fig_index+1) * 10)
+        spike_time_masked = spike_time[mask]
+        group_index_maksed = group_index[mask]
+
+        plt.figure(figsize=(12, 4))
+        plt.plot(spike_time_masked / ms, group_index_maksed, '.', color='r', markersize=1)
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Group index')
+        plt.title('Spike raster (grouped)')
+        plt.savefig(os.path.join(save_path, f'rastor_plot{fig_index}.png'), format='png', dpi=600)
+        plt.close('all')
+
+
 # def plot_v(ESM, ISM, neuron=13):
 #     plt.rcParams["figure.figsize"] = (20, 6)
 #     cnt = -50000  # tail
@@ -22,17 +85,25 @@ def plot_w(S1M):
 #     ylabel('v')
 #     show()
 
-def plot_rates(ERM):
-    plt.rcParams["figure.figsize"] = (20, 6)
-    plot(ERM.t / ms, ERM.smooth_rate(window='flat', width=200* ms) * Hz, color='r')
-    ylabel('Rate')
-    show()
+def process_spikes(model):
+    monitor = model['excitatory_spike']
+    groups = model.neuron_group
+    spike_trains = monitor.spike_trains()
 
-def plot_spikes(ESP):
-    plt.rcParams["figure.figsize"] = (20, 6)
-    plot(ESP.t / ms, ESP.i, '.r')
-    ylabel('Neuron index')
-    show()
+    times_list = []
+    group_id_list = []
+    for g in range(len(groups)):
+        neuron_list = groups[g]
+        if len(neuron_list) == 0:
+            continue
+        group_times = np.concatenate([spike_trains[i] for i in neuron_list])
+        group_ids = np.full_like(group_times, g, dtype=int)
+        times_list.append(group_times)
+        group_id_list.append(group_ids)
+
+    all_times = np.concatenate(times_list)
+    all_groups = np.concatenate(group_id_list)
+    return all_times, all_groups
 
 def ColoredGaussianDataset(D=64, top_eigs=[7, 6, 5, 4], low=(0, 0.5), seed=42):
     D = D
@@ -54,7 +125,8 @@ def ColoredGaussianDataset(D=64, top_eigs=[7, 6, 5, 4], low=(0, 0.5), seed=42):
 def smooth_rate_per_neuron(spike_i, spike_t, num_neurons,
                            stim_start, stim_duration,
                            dt=0.1,
-                           sigma=0.1
+                           sigma=0.1,
+                           remove_burst=False
                            ):
 
     for stimulation in range(len(stim_start)):
@@ -83,7 +155,7 @@ def smooth_rate_per_neuron(spike_i, spike_t, num_neurons,
             std_rate = valid_rate.std()
             threshold = mean_rate + 0.5 * std_rate
             avalanche_average = inst_rate[inst_rate>threshold].mean()
-            inst_rate[inst_rate > threshold] -= avalanche_average
+            inst_rate[inst_rate > threshold] -= avalanche_average if remove_burst else 0
             inst_rate[inst_rate < 0] = 0
 
             rates_sm[nid, :] = np.convolve(inst_rate, gauss, mode='same')
@@ -94,3 +166,32 @@ def smooth_rate_per_neuron(spike_i, spike_t, num_neurons,
         # plt.show()
         return rates_avg, times, rates_sm
 
+def compute_rates(spike_mon, n_per_group, n_groups, dt_bin=10*ms, smooth_width=200*ms):
+    t_all = spike_mon.t
+    i_all = spike_mon.i
+
+    group_all = i_all // n_per_group
+
+    t_all_ms = t_all / ms
+    dt_bin_ms = dt_bin / ms
+    t_max = t_all_ms.max()
+    bins = np.arange(0, t_max + dt_bin_ms, dt_bin_ms)
+
+    counts = np.zeros((n_groups, len(bins)-1))
+    for g in range(n_groups):
+        mask = (group_all == g)
+        counts[g], _ = np.histogram(t_all_ms[mask], bins=bins)
+
+    bin_sec = dt_bin_ms / 1000.0
+    rates = counts / (n_per_group * bin_sec)  # shape (n_groups, n_time_bins)
+
+    window_bins = int((smooth_width/ms) / dt_bin_ms)
+    if window_bins > 1:
+        kernel = np.ones(window_bins) / window_bins
+        rates_smooth = np.array([np.convolve(r, kernel, mode='same') for r in rates])
+    else:
+        rates_smooth = rates
+    bin_centers_ms = (bins[:-1] + bins[1:]) / 2.0
+    t_seconds = bin_centers_ms / 1000.0
+
+    return t_seconds, rates_smooth.mean(axis=0)
